03bca8b841542041914c55901cc327a75972600b
thimovandermeer <thimovandermeer@gmail.com>
2021-01-25 11:10:10 +0100

    CI-CD added

---
+++ testResponse.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Thimo Van der meer on 20/01/2021.
+//
+
+#include "Response.h"
+#include "Request.h"
+#include "Requestconfig.h"
+
+int main()
+{
+	Response Response();
+
+	checkMethod(request, requestconfig);
+
+}
