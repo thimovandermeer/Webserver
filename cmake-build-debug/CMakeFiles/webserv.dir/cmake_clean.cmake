787ce6d11503de7d211bd4761213d4b11cd4a9bb
jbennink <jbennink@student.codam.nl>
2021-02-05 11:08:16 +0100

    put some files in .gitignore, but that also removed run configs, so undoing that now

---
+++ cmake-build-debug/CMakeFiles/webserv.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/webserv.dir/srcs/Cluster/serverCluster.cpp.o"
+  "CMakeFiles/webserv.dir/srcs/Request/request.cpp.o"
+  "CMakeFiles/webserv.dir/srcs/Response/Response.cpp.o"
+  "CMakeFiles/webserv.dir/srcs/Response/ResponseHeader.cpp.o"
+  "CMakeFiles/webserv.dir/srcs/Server/location.cpp.o"
+  "CMakeFiles/webserv.dir/srcs/Server/parser.cpp.o"
+  "CMakeFiles/webserv.dir/srcs/Server/server.cpp.o"
+  "CMakeFiles/webserv.dir/srcs/Utils/utils.cpp.o"
+  "CMakeFiles/webserv.dir/srcs/webserv.cpp.o"
+  "webserv"
+  "webserv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/webserv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
