dc865a7709abee0f87a4a2abbf5237f398b77e15
jbennink <jbennink@student.codam.nl>
2021-04-13 17:08:39 +0200

    final update to run tests

---
+++ runtest.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	./tester "http://localhost:8080"
+elif [[ "$OSTYPE" == "Ubuntu"* ]]; then
+	./ubuntu_tester "http://localhost:8080"
+else
+	echo "current OS is $OSTYPE, this only runs on OS X or Ubuntu"
+fi
