846df2c4fd58aec4194c0cd8f1dc5cde3bbc3f5c
merelmourik <merelmourik@gmail.com>
2021-05-07 14:07:24 +0200

    and change in connection.cpp

--- srcs/Server/connection.cpp
+++ srcs/Server/connection.cpp
@@ -52,6 +52,7 @@ const unsigned long&	connection::getTimeLastRead() const
 
 const long&			connection::getAcceptFd() const
 {
+    // -> merelvwaar en hoe wordt deze accept fd bepaald?
 	return (this->_acceptFd);
 }
 
